55fe33becc680ff251d36a87d31daabd0d819354
msmashraful <msmashraful@gmail.com>
2025-05-31 00:36:37 +0600

    first time

---
+++ sample.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("https://www.daraz.com.bd/#?")
+driver.maximize_window()
+tittle = driver.find_element(By.XPATH, '//*[@id="171444191"]/div[2]/p').text
+
+
+link = driver.find_element(By.XPATH, '//*[@id="171444191"]').get_attribute('href')
+
+print(tittle, link)
+time.sleep(50)  # Let the user actually see something!
